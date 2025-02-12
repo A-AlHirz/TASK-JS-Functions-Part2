0cd8589603f3ac29ec50f4066228f781a0beeb87
Abdullah Alherz <abdherz@hotmail.com>
2025-02-12 10:48:47 +0300

    functionspart2

--- functions.js
+++ functions.js
@@ -7,7 +7,7 @@
  * greet("Hamza") logs "Hello Hamza"
  */
 function greet(name) {
-  // Your code here
+  console.log(`Hello ${name}`);
 }
 
 /**
@@ -20,7 +20,7 @@ function greet(name) {
  * isOdd(10) -> false
  */
 function isOdd(n) {
-  // Your code here
+  return n % 2 !== 0;
 }
 
 /**
@@ -33,7 +33,7 @@ function isOdd(n) {
  * oddsSmallerThan(15) -> 7
  */
 function oddsSmallerThan(n) {
-  // Your code here
+  return Math.floor(n / 2);
 }
 
 /**
@@ -47,7 +47,7 @@ function oddsSmallerThan(n) {
  * squareOrDouble(9) -> 81
  */
 function squareOrDouble(n) {
-  // Your code here
+  return isOdd(n) ? n * n : n * 2;
 }
 
 module.exports = {
